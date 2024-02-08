6d1a0c4cd8ae876989aebb65751afde38120326e
cohnor08 <cohnorwalsh@icloud.com>
2024-02-08 15:16:02 +1300

    Update Double.py

--- Double.py
+++ Double.py
@@ -1,11 +1,13 @@
 """ this program takes a number as input and doubles it """
 
-# take number as a float
 
-try:
-    number = float(input("Please Enter your Number "))
-except ValueError:
-    print("Wrong Answer")
+keep_asking = True
+while keep_asking:
+    # take number as a float
+    try:
+        number = float(input("Please Enter your Number "))
+        keep_asking = False
+    except ValueError:
+        print("Wrong Answer")
 
 print(number * 2)
-
