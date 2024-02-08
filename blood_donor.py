b0d083dd1e4a56d944a6687e6cd9a2323cc5649d
cohnor08 <cohnorwalsh@icloud.com>
2024-02-09 10:40:28 +1300

    Update blood_donor.py

--- blood_donor.py
+++ blood_donor.py
@@ -1,2 +1,19 @@
 ''' this program assesses whether or not a blood donor is eligible for blood'''
 
+keep_asking = True
+while keep_asking == True:
+
+    try:
+
+
+        age = float(input("Please Enter Your Age"))
+        weight = float(input("Please Enter Your Weight"))
+
+        if age >=16 and weight >=50:
+            print("You Are Eligible")
+        else:
+            print("You Are Not ELigible")
+
+        keep_asking = False
+    except ValueError:
+        print("Try again")
