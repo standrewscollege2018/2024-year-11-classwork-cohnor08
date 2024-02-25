ab43408b8d502ce528871c6b749457695f3be435
cohnor08 <cohnorwalsh@icloud.com>
2024-02-26 09:36:51 +1300

    raffle without error catching

---
+++ Raffle.py
@@ -0,0 +1,31 @@
+""" This program takes a input of names and then randomly picks one for the raffle"""
+
+# inputs the package random
+import random
+
+# creates a list called names
+names = []
+
+# get prize
+prize = input("What is the prize for your raffle ")
+# welcome message
+print("Please enter the names who will be participating, and press enter!")
+# repeat forever untill keep_asking equals false
+keep_asking = True
+while keep_asking:
+    # gets a input
+    get_name = input("")
+
+    # if end is entered break
+    if get_name.lower() == "end":
+        keep_asking = False
+
+    # put inputted names into list
+    else:
+        names.append(get_name)
+
+# random library
+r = random.randint(0, len(names) - 1)
+
+# Print the random name from list
+print(f"Good Job {names[r]}, you won {prize}! ")
